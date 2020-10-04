3a5375cb34ed7b3103cd4fe81c044ad3bd7b2a30
Leonardo Santos <leotsantos@hotmail.com>
2020-10-04 17:46:48 -0300

    Added test cases for deposit and get balance for existing account

--- src/test/java/com/github/leotsantos/apidemo/requests/AccountControllerTest.java
+++ src/test/java/com/github/leotsantos/apidemo/requests/AccountControllerTest.java
@@ -2,7 +2,10 @@ package com.github.leotsantos.apidemo.requests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.github.leotsantos.apidemo.domain.EventBody;
 import com.google.gson.Gson;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,7 +28,7 @@ public class AccountControllerTest {
     private MockMvc mockMvc;
 	
 	@Test
-	public void resetStatus_shouldReturnOk() throws Exception {
+	public void test01_resetStatus_shouldReturnOk() throws Exception {
 		String response = mockMvc.perform(MockMvcRequestBuilders.post("/reset"))
 	            .andExpect(MockMvcResultMatchers.status().isOk())
 	            .andReturn()
@@ -33,7 +37,7 @@ public class AccountControllerTest {
 	}
 	
 	@Test
-	public void getBalanceInvalidAccount_shouldReturnNotFound() throws Exception {
+	public void test02_getBalanceInvalidAccount_shouldReturnNotFound() throws Exception {
 		String response = mockMvc.perform(MockMvcRequestBuilders.get("/balance?account_id=1234"))
 	            .andExpect(MockMvcResultMatchers.status().isNotFound())
 	            .andReturn()
@@ -42,7 +46,7 @@ public class AccountControllerTest {
 	}
 	
 	@Test
-	public void createAccountWithInitialBalance_shouldReturnOk() throws Exception {
+	public void test03_createAccountWithInitialBalance_shouldReturnOk() throws Exception {
 		
 		EventBody body = new EventBody("deposit", null, "100", 10);
 		
@@ -58,6 +62,37 @@ public class AccountControllerTest {
 	            .getContentAsString();	
 		
 		Assert.assertEquals("{\"destination\":{\"id\":\"100\",\"balance\":10}}", response);
+	}
+	
+	@Test
+	public void test04_makeDepositOnExistingAccount_shouldReturnOk() throws Exception {
+		
+		EventBody body = new EventBody("deposit", null, "100", 10);
+		
+		Gson gson = new Gson();
+	    String json = gson.toJson(body);
+		
+		String response = mockMvc.perform(MockMvcRequestBuilders.post("/event")
+				.contentType(MediaType.APPLICATION_JSON)
+	            .content(json))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+	            .andReturn()
+	            .getResponse()
+	            .getContentAsString();	
+		
+		Assert.assertEquals("{\"destination\":{\"id\":\"100\",\"balance\":20}}", response);
+	}
+	
+	@Test
+	public void test05_getBalanceForExistingAccount_shouldReturnOk() throws Exception {
+		
+		String response = mockMvc.perform(MockMvcRequestBuilders.get("/balance?account_id=100"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+	            .andReturn()
+	            .getResponse()
+	            .getContentAsString();	
+		
+		Assert.assertEquals("20", response);
 	}
 
 }
